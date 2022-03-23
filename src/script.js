d5dcce212df1475354de15fdfa6e5001d5a34857
Paul Montgomery <pmontgomery@fvfh51faq6lw.domain>
2022-03-23 06:46:30 -0700

    it works

--- src/script.js
+++ src/script.js
@@ -1,112 +1,195 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { CSS2DRenderer, CSS2DObject } from "three-css2drender";
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+
+// import { CSS2DRenderer, CSS2DObject } from "three-css2drender";
+import {CSS3DRenderer, CSS3DSprite, CSS3DObject} from 'three-css3d';
+
+let campaign_impact = "<table class='tbl' border='1'><th colspan='2'><div class='head'>campaign_impact</tr><tr><td><div class='c2'>campaign_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c3'>click_code</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c3'>mktg_tag</div></td><td><div>varchar(255)</div></td></tr></table>"
+let campaigns = "<table class='tbl' border='1'><th colspan='2'><div class='head'>campaigns</tr><tr><td><div class='c2'>campaign_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>campaign_name</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>campaign_type</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>date</div></td><td><div>datetime</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr></table>"
+let click_names = "<table class='tbl' border='1'><th colspan='2'><div class='head'>click_names</tr><tr><td><div class='c3'>click_code</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>click_name</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>click_str</div></td><td><div>char(1)</div></td></tr><tr><td><div class='c1'>click_type</div></td><td><div>char(1)</div></td></tr><tr><td><div class='c1'>count_it</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>rec_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>url</div></td><td><div>varchar(255)</div></td></tr></table>"
+let click_sum = "<table class='tbl' border='1'><th colspan='2'><div class='head'>click_sum</tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>month</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>vector_sum</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>year</div></td><td><div>int(11)</div></td></tr></table>"
+let clicks = "<table class='tbl' border='1'><th colspan='2'><div class='head'>clicks</tr><tr><td><div class='c3'>click_code</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>click_dt</div></td><td><div>datetime</div></td></tr><tr><td><div class='c2'>click_str</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>cookie</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c3'>mktg_tag</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c4'>portal_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>session_id</div></td><td><div>int(11)</div></td></tr></table>"
+let demographics = "<table class='tbl' border='1'><th colspan='2'><div class='head'>demographics</tr><tr><td><div class='c2'>lob</div></td><td><div>char(1)</div></td></tr><tr><td><div class='c1'>age</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>birth_dt</div></td><td><div>datetime</div></td></tr><tr><td><div class='c2'>brand</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>gender</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>grgr_id</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>grgr_name</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>parent_group</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c4'>portal_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>product</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>start_date</div></td><td><div>datetime</div></td></tr><tr><td><div class='c2'>state</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>term_date</div></td><td><div>datetime</div></td></tr></table>"
+let registration = "<table class='tbl' border='1'><th colspan='2'><div class='head'>registration</tr><tr><td><div class='c1'>elf_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>email_address</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>reg_dt</div></td><td><div>datetime</div></td></tr><tr><td><div class='c2'>session_id</div></td><td><div>int(11)</div></td></tr></table>"
+let segment = "<table class='tbl' border='1'><th colspan='2'><div class='head'>segment</tr><tr><td><div class='c2'>lob</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>brand</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>grgr_id</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>last_login</div></td><td><div>datetime</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>month</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>parent_group</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c4'>portal_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>reg_dt</div></td><td><div>datetime</div></td></tr><tr><td><div class='c1'>segment_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c2'>state</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c2'>year</div></td><td><div>int(11)</div></td></tr></table>"
+let sessions = "<table class='tbl' border='1'><th colspan='2'><div class='head'>sessions</tr><tr><td><div class='c1'>device</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c8'>member_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c3'>mktg_tag</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c7'>person_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c4'>portal_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>sesion_id</div></td><td><div>int(11)</div></td></tr><tr><td><div class='c1'>session_dt</div></td><td><div>datetime</div></td></tr></table>"
+let user = "<table class='tbl' border='1'><th colspan='2'><div class='head'>user</tr><tr><td><div class='c1'>c</div></td><td><div>char(1)</div></td></tr><tr><td><div class='c2'>date</div></td><td><div>datetime</div></td></tr><tr><td><div class='c1'>email</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>goat</div></td><td><div>varchar(255)</div></td></tr><tr><td><div class='c1'>name</div></td><td><div>varchar(255)</div></td></tr></table>"
 
 
-const canvas = document.getElementById("target");
-const HEIGHT = window.innerHeight * 0.8;
-function main() {
+// const canvas = document.getElementById("target");
 
-    const renderer = new THREE.WebGLRenderer({canvas});
-    renderer.setSize(window.innerWidth, HEIGHT )
 
-    const fov = 75;
-    const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 25;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 4;
-  
-    // const controls = new THREE.OrbitControls(camera, canvas);
-    const controls = new OrbitControls(camera, renderer.domElement)
 
-    controls.target.set(0, 0, 0);
-    controls.update();
-  
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color('white');
-  
-    function addLight(...pos) {
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(...pos);
-      scene.add(light);
-    }
-    addLight(-1, 2, 4);
-    addLight( 1, -1, -2);
-  
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-  
-    function makeInstance(geometry, color, x, y, z) {
-      const material = new THREE.MeshPhongMaterial({
-        color,
-        opacity: 0.5,
-        transparent: true,
-      });
-  
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-  
-      cube.position.set(x, y, z);
-  
-      return cube;
-    }
-  
-    function hsl(h, s, l) {
-      return (new THREE.Color()).setHSL(h, s, l);
-    }
-  
-    {
-      const d = 0.8;
-      makeInstance(geometry, hsl(0 / 8, 1, .5), -d, -d, -d);
-      makeInstance(geometry, hsl(1 / 8, 1, .5),  d, -d, -d);
-      makeInstance(geometry, hsl(2 / 8, 1, .5), -d,  d, -d);
-      makeInstance(geometry, hsl(3 / 8, 1, .5),  d,  d, -d);
-      makeInstance(geometry, hsl(4 / 8, 1, .5), -d, -d,  d);
-      makeInstance(geometry, hsl(5 / 8, 1, .5),  d, -d,  d);
-      makeInstance(geometry, hsl(6 / 8, 1, .5), -d,  d,  d);
-      makeInstance(geometry, hsl(7 / 8, 1, .5),  d,  d,  d);
-    }
-  
-    function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
-  
-    let renderRequested = false;
-  
-    function render() {
-      renderRequested = undefined;
-  
-      if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-  
-      renderer.render(scene, camera);
-    }
-    render();
-  
-    function requestRenderIfNotRequested() {
-      if (!renderRequested) {
-        renderRequested = true;
-        requestAnimationFrame(render);
-      }
-    }
-  
-    controls.addEventListener('change', requestRenderIfNotRequested);
-    window.addEventListener('resize', requestRenderIfNotRequested);
+let distance = 6000
+
+  const months = []
+  function When (id, display, distance) {
+    this.id = id
+    this.display = display
+    this.distance = distance
   }
-  
-  main();
+
+  const timeline = () => {
+  // +6000 'units' is what Z is set to in the three.js code
+    const distanceLikurt_betweenMonths = 6000 / 24
+    distance = 6000
+    for (var i = 0; i < 24; i++) {
+      let d = new Date()
+      d.setMonth((d.getMonth() - 12) + i)
+      months.push(new When(i, formatDate(d), distance))
+      distance -= distanceLikurt_betweenMonths
+    }
+  }
+
+  // const showDates = () => {
+  //   for (x in months) {
+  //     let o = months[x]
+  //     console.log(o.id + ' -> ' + o.display + ' -> ' + o.distance)
+  //   }
+  // }
+
+  const formatDate = (d) => {
+    let year = 1900 + d.getYear()
+    let month = 1 + d.getMonth()
+    if (month < 10) {
+      month = '0' + month
+    }
+    let date = d.getDate()
+    if (date < 10) {
+      date = '0' + date
+    }
+    let yyyymmdd = year + '/' + month + '/' + date
+    return yyyymmdd
+  }
+
+  timeline()
+
+// ------------------------------------
+
+  var height = 500
+  var camera, scene, renderer
+  var controls
+
+  var objects = []
+  init()
+  animate()
+
+  function createTable () {
+    let html = "<table class='main'>"
+    html += '<tr>'
+    html += '<td valign=top>' + sessions + '</td>'
+
+    html += '<td valign=top>' + campaigns + '</td>'
+    html += '<td valign=top>' + click_names + '</td>'
+    html += '<td valign=top rowspan=2>' + demographics + '</td>'
+
+    html += '</tr><tr>'
+    html += '<td valign=top rowspan=2>' + segment + '</td>'
+
+    html += '<td valign=top>' + click_sum + '</td>'
+    html += '<td valign=top>' + registration + '</td>'
+    html += '</tr><tr>'
+
+    html += '<td valign=top>' + clicks + '</td>'
+    html += '<td valign=top>' + user + '</td>'
+    html += '<td valign=top>' + campaign_impact + '</td>'
+
+    html += '</tr></html>'
+
+    return html
+  }
+
+  function addTable (table, distance) {
+    var bunny = document.createElement('div')
+
+    var details = document.createElement('div')
+    details.className = 'details'
+
+    details.innerHTML = table
+    bunny.appendChild(details)
+
+    var o2 = new CSS3DObject(bunny)
+    scene.add(o2)
+
+    o2.position.x = 300// 500 + (Math.floor(Math.random() * 1000))
+    o2.position.y = 300// -500 + (Math.floor(Math.random() * 1000))
+    o2.position.z = distance// -500 + (Math.floor(Math.random() * 4000))
+  }
+
+  function addWhen (index) {
+    let w = months[index]
+    // console.log('addWhen: ' + index + 'w ' + w)
+
+    var thing = document.createElement('div')
+    var details = document.createElement('div')
+    details.className = 'timeline'
+    details.innerHTML = w.display
+    thing.appendChild(details)
+
+    // var o2 = new THREE.CSS3DObject(thing)
+    var o2 = new CSS3DObject(thing)
+    scene.add(o2)
+
+    o2.position.x = -200// + (Math.floor(Math.random() * 1000))
+    o2.position.y = -200// + (Math.floor(Math.random() * 1000))
+    o2.position.z = w.distance
+  }
+
+  function init () {
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / height, 1, 10000)
+    camera.position.z = 10000
+    scene = new THREE.Scene()
+
+    // months[22].id = '0'
+    // months[22].display = 'ZOOM'
+    // months[22].distance = 400
+
+    for (var i = 23; i > 0; i--) {
+      // months[i].display = months[i].distance + ' | ' + months[i].id
+      addWhen(i)
+    }
+
+    let html = createTable()
+    addTable(html, months[0].distance)
+    addTable(html, months[9].distance)
+    addTable(html, months[15].distance)
+
+    renderer = new CSS3DRenderer()
+    renderer.setSize(window.innerWidth, height)
+    renderer.domElement.style.position = 'absolute'
+    document.getElementById('container').appendChild(renderer.domElement)
+    // canvas.appendChild(renderer.domElement)
+
+    controls = new TrackballControls(camera, renderer.domElement)
+    // controls = new OrbitControls(camera, renderer.domElement)
+
+    controls.rotateSpeed = 0.5
+    controls.minDistance = 500
+    controls.maxDistance = 6000
+    controls.addEventListener('change', render)
+
+    window.addEventListener('resize', onWindowResize, false)
+
+//    cube()
+
+    render()
+  }
+
+  function onWindowResize () {
+    camera.aspect = window.innerWidth / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, height)
+    render()
+  }
+
+  function animate () {
+    requestAnimationFrame(animate)
+    controls.update()
+    console.log("animate")
+  }
+
+  function render () {
+    renderer.render(scene, camera)
+    console.log("redner")
+  }
--- src/script2.js
+++ src/script2.js
@@ -1,80 +1,113 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// import { CSS2DRenderer, CSS2DObject } from "three-css2drender";
+import {CSS3DRenderer, CSS3DSprite, CSS3DObject} from 'three-css3d';
 
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-const scene = new THREE.Scene()
-// scene.add(new THREE.AxesHelper(5))
-
-scene.background = new THREE.Color( 0xa0a0a0 );
-scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
-
-const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 20, 0 );
-scene.add( hemiLight );
-
-const dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set( 3, 10, 10 );
-dirLight.castShadow = true;
-dirLight.shadow.camera.top = 2;
-dirLight.shadow.camera.bottom = - 2;
-dirLight.shadow.camera.left = - 2;
-dirLight.shadow.camera.right = 2;
-dirLight.shadow.camera.near = 0.1;
-dirLight.shadow.camera.far = 40;
-scene.add( dirLight );
-
-
-
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    100
-)
-camera.position.z = 2
-// alert("w: " + window.innerWidth + " h: " + window.innerHeight + " and " +  )
 
 const canvas = document.getElementById("target");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight)
+const HEIGHT = window.innerHeight * 0.8;
+function main() {
 
-const controls = new OrbitControls(camera, renderer.domElement)
+    const renderer = new THREE.WebGLRenderer({canvas});
+    renderer.setSize(window.innerWidth, HEIGHT )
 
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 25;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 4;
+  
+    // const controls = new THREE.OrbitControls(camera, canvas);
+    const controls = new OrbitControls(camera, renderer.domElement)
 
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-
-const loader = new GLTFLoader();
-
-loader.load( 'Xbot.glb', function ( gltf ) {
-	scene.add( gltf.scene );
-}, undefined, function ( error ) {
-	console.error( error )
-} );
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-    render()
-}
-function render() {
-    renderer.render(scene, camera)
-}
-animate()
-
+    controls.target.set(0, 0, 0);
+    controls.update();
+  
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('white');
+  
+    function addLight(...pos) {
+      const color = 0xFFFFFF;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(...pos);
+      scene.add(light);
+    }
+    addLight(-1, 2, 4);
+    addLight( 1, -1, -2);
+  
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  
+    function makeInstance(geometry, color, x, y, z) {
+      const material = new THREE.MeshPhongMaterial({
+        color,
+        opacity: 0.5,
+        transparent: true,
+      });
+  
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+  
+      cube.position.set(x, y, z);
+  
+      return cube;
+    }
+  
+    function hsl(h, s, l) {
+      return (new THREE.Color()).setHSL(h, s, l);
+    }
+  
+    {
+      const d = 0.8;
+      makeInstance(geometry, hsl(0 / 8, 1, .5), -d, -d, -d);
+      makeInstance(geometry, hsl(1 / 8, 1, .5),  d, -d, -d);
+      makeInstance(geometry, hsl(2 / 8, 1, .5), -d,  d, -d);
+      makeInstance(geometry, hsl(3 / 8, 1, .5),  d,  d, -d);
+      makeInstance(geometry, hsl(4 / 8, 1, .5), -d, -d,  d);
+      makeInstance(geometry, hsl(5 / 8, 1, .5),  d, -d,  d);
+      makeInstance(geometry, hsl(6 / 8, 1, .5), -d,  d,  d);
+      makeInstance(geometry, hsl(7 / 8, 1, .5),  d,  d,  d);
+    }
+  
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+  
+    let renderRequested = false;
+  
+    function render() {
+      renderRequested = undefined;
+  
+      if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+  
+      renderer.render(scene, camera);
+    }
+    render();
+  
+    function requestRenderIfNotRequested() {
+      if (!renderRequested) {
+        renderRequested = true;
+        requestAnimationFrame(render);
+      }
+    }
+  
+    controls.addEventListener('change', requestRenderIfNotRequested);
+    window.addEventListener('resize', requestRenderIfNotRequested);
+  }
+  
+  main();
